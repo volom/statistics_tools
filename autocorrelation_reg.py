45342723160c80eaaf68ec7ac705464d3706fd44
Volodymyr <83507366+volom@users.noreply.github.com>
2023-02-15 17:03:23 +0200

    Update autocorrelation_reg.py

--- autocorrelation_reg.py
+++ autocorrelation_reg.py
@@ -2,6 +2,10 @@ import numpy as np
 import pandas as pd
 from sklearn.datasets import load_boston
 import statsmodels.api as sm
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.stats.diagnostic import acorr_ljungbox
+from statsmodels.stats.diagnostic import het_breuschpagan
+
 
 # load the Boston Housing dataset
 boston = load_boston()
@@ -18,5 +22,28 @@ model = sm.OLS(y, sm.add_constant(X)).fit()
 # calculate the Durbin-Watson test statistic for autocorrelation
 dw = sm.stats.stattools.durbin_watson(model.resid)
 
+# calculate the autocorrelation function (ACF)
+acf = sm.tsa.stattools.acf(model.resid)
+
+# calculate the partial autocorrelation function (PACF)
+pacf = sm.tsa.stattools.pacf(model.resid)
+
+# perform the Ljung-Box test for autocorrelation
+lb = acorr_ljungbox(model.resid, lags=[10], boxpierce=False)
+
+# perform the Breusch-Godfrey test for autocorrelation
+bg = het_breuschpagan(model.resid, model.model.exog)
+
+
+
 # print the test statistic to the console
-print("Durbin-Watson test statistic:", dw)
+print("Durbin-Watson test statistic: ", dw)
+print("Autocorrelation Function (ACF): ")
+plot_acf(model.resid)
+plt.show()
+print("Partial Autocorrelation Function (PACF): ")
+plot_pacf(model.resid)
+plt.show()
+print("Ljung-Box test results: ", lb)
+print("Breusch-Godfrey test results: ", bg)
+
