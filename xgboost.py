d030802a01e9f7b2a71cf80db53453ff984b89a4
Volodymyr <83507366+volom@users.noreply.github.com>
2023-02-17 13:19:03 +0200

    xgboost

---
+++ xgboost.py
@@ -0,0 +1,41 @@
+import xgboost as xgb
+import numpy as np
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+
+# Generate some example data
+X, y = make_classification(n_samples=1000, n_features=10, n_informative=5, n_redundant=0, random_state=42)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Convert the data to a DMatrix format
+dtrain = xgb.DMatrix(X_train, label=y_train)
+dtest = xgb.DMatrix(X_test, label=y_test)
+
+# Set the hyperparameters
+params = {
+    "max_depth": 3,
+    "eta": 0.1,
+    "objective": "binary:logistic",
+    "eval_metric": "logloss"
+}
+
+# Train the model
+num_rounds = 100
+model = xgb.train(params, dtrain, num_rounds)
+
+# Make predictions on the test set
+y_pred = model.predict(dtest)
+
+# Evaluate the model
+from sklearn.metrics import accuracy_score, roc_auc_score
+print("Accuracy:", accuracy_score(y_test, np.round(y_pred)))
+print("ROC AUC score:", roc_auc_score(y_test, y_pred))
+
+# Print the feature importance
+importances = model.get_score(importance_type='gain')
+importances = sorted(importances.items(), key=lambda x: x[1], reverse=True)
+print("Feature importance:")
+for i in importances:
+    print(i)
